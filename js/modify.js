1b342ad41f31a37f19bcda74eff2fad3910633cb
Sagarudeshi <ssu9@njit.edu>
2017-04-19 13:37:00 -0400

    Optimized js & css restored. Index file improved.

---
+++ js/modify.js
@@ -0,0 +1,41 @@
+
+
+	$(".overlay1").bind("click",function(){
+	
+		/* $("#navigation").attr("class","affix");
+		
+		alert($("#navigation").attr("class")); */
+		
+		
+		$(".fadeInUp").css("visibility","visible");
+		
+		/* if($("#navigation").hasClass("affix")){
+			alert("hello");
+		}
+		else if($("#navigation").hasClass("affix-top")){
+			$("#navigation").attr("class","affix");
+			alert("NO hello");
+		}	 */
+		});
+	
+	
+	/* $(".mfp-close").bind("click",function(){
+	
+		$("#navigation").attr("class","affix");
+		
+		
+		
+		/* alert($("#navigation").attr("class")); */
+		
+		/* $(".fadeInUp").css("visibility","visible"); */
+		
+		/* }); */ 
+		
+	/* alert($("#navigation").attr("class"));
+		
+	 $(".overlay1").bind("click",function(){
+		
+		console.log("Hi  jndfjndfjndfss");
+		
+		
+	}); */	 
